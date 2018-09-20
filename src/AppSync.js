b333ca9d7ebce787235d4df467944975a01033f8
Rafael Madrid <rafael.madrid92@gmail.com>
2018-09-20 06:57:43 -0600

    Adding aws appsync sdk.

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://egc4gyyhybg2jasvwf6hjqhhra.appsync-api.us-east-1.amazonaws.com/graphql",
+    "region": "us-east-1",
+    "authenticationType": "AMAZON_COGNITO_USER_POOLS",
+    "apiKey": "null"
+}
--- src/boot/setup.js
+++ src/boot/setup.js
@@ -3,8 +3,24 @@ import App from "../App";
 import Amplify from 'aws-amplify';
 import aws_exports from '../aws-exports';
 import { Root } from 'native-base';
+import AWSAppSyncClient from "aws-appsync";
+import { Rehydrated } from 'aws-appsync-react';
+import { graphql, ApolloProvider, compose } from 'react-apollo';
+import AppSyncConfig from '../AppSync';
+import { Auth } from 'aws-amplify';
 
 Amplify.configure(aws_exports);
+
+const client = new AWSAppSyncClient({
+  url: AppSyncConfig.graphqlEndpoint,
+  region: AppSyncConfig.region,
+  auth: {
+    type: AppSyncConfig.authenticationType,
+    apiKey: AppSyncConfig.apiKey,
+    jwtToken: async () => (await Auth.currentSession()).getAccessToken().getJwtToken(),
+  }
+});
+
 
 export default class Setup extends React.Component {
   constructor() {
